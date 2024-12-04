ef2bd94ab57b90919671858a72620b312829ab54
sinofseven <em.s.00001@gmail.com>
2024-12-05 05:07:56 +0900

    fix terraform version

--- terraform.tf
+++ terraform.tf
@@ -3,12 +3,12 @@
 # ================================================================
 
 terraform {
-  required_version = "~> 1.9"
+  required_version = "1.10.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.72"
+      version = "5.78.0"
     }
   }
 
